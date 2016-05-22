6f9f25d4d654f853c27476afae2317b5f50ef55d
windog <windiscall@gmail.com>
2016-05-22 17:20:13 +0800

    layout and adapter for news title

---
+++ app/src/main/java/com/test/windy/fragmentbestpractice/adapter/NewsAdapter.java
@@ -0,0 +1,55 @@
+package com.test.windy.fragmentbestpractice.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.test.windy.fragmentbestpractice.R;
+import com.test.windy.fragmentbestpractice.bean.News;
+
+import java.util.List;
+
+/**
+ * Created by windog on 2016/5/22.
+ */
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    private int resourceId;
+
+    /**
+     * Constructor
+     *
+     * @param context  The current context.
+     * @param resource The resource ID for a layout file containing a TextView to use when
+     *                 instantiating views.
+     * @param objects  The objects to represent in the ListView.
+     */
+    public NewsAdapter(Context context, int resource, List<News> objects) {
+        super(context, resource, objects);
+        resourceId = resource;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param position
+     * @param convertView
+     * @param parent
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        News news = getItem(position);
+        View view;
+        if (convertView != null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+        } else {
+            view = convertView;
+        }
+        TextView newTitleText = (TextView) view.findViewById(R.id.news_title);
+        newTitleText.setText(news.getTitle());
+        return view;
+    }
+}
---
+++ app/src/main/java/com/test/windy/fragmentbestpractice/bean/News.java
@@ -0,0 +1,25 @@
+package com.test.windy.fragmentbestpractice.bean;
+
+/**
+ * Created by windog on 2016/5/22.
+ */
+public class News {
+    private String title; //新闻标题
+    private String content; //新闻内容
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
